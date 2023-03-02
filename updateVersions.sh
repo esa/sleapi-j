99d98b9b6c227c8c67bf2ec0d6a60c424628f1a0
Laur Tooming <laur.tooming@cgi.com>
2023-03-02 10:13:03 +0200

    Version 5.1.4
    Removed SNAPSHOT from version

--- updateVersions.sh
+++ updateVersions.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mvn org.eclipse.tycho:tycho-versions-plugin:1.3.0:set-version -DnewVersion=5.1.4-SNAPSHOT
+mvn org.eclipse.tycho:tycho-versions-plugin:1.3.0:set-version -DnewVersion=5.1.4
